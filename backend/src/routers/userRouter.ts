add8678c33d7a7f6392f443567b8750b37946835
Pranav Talwar <talwar.pranav123@gmail.com>
2020-06-23 18:20:27 +0530

    added error handling to userrouter

--- backend/src/routers/userRouter.ts
+++ backend/src/routers/userRouter.ts
@@ -1,10 +1,10 @@
 import express, { Router, Request, Response } from 'express'
-import User from '../models/User'
+import User, { IUser } from '../models/User'
 
 const router: Router = express.Router()
 
 router.post('/users', async (req: Request, res: Response) => {
-    const user: any = new User(req.body)
+    const user: IUser = new User(req.body)
 
     try {
         await user.save()
@@ -15,8 +15,8 @@ router.post('/users', async (req: Request, res: Response) => {
 })
 
 router.get('/users', async (req: Request, res: Response) => {
-    const users: Object[] = await User.find({})
     try {
+        const users: IUser[] = await User.find({})
         res.status(200).send(users)
     } catch(error) {
         res.status(500).send()
@@ -24,30 +24,40 @@ router.get('/users', async (req: Request, res: Response) => {
 })
 
 router.get('/users/:id', async (req: Request, res:Response) => {
-    const id: string = req.params.id
-    const user: any = await User.findById(id)
-    if(!user) {
-        return res.status(404).send()
+    const { id }: { id: string } = req.params as { id: string }
+    try {
+        const user: IUser | null = await User.findById(id)
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch(error) {
+        res.status(500).send()
     }
-    res.send(user)
+    
 })
 
 router.patch('/users/:id', async (req: Request, res: Response) => {
-    const id: string = req.params.id
-    const user: any = await User.findById(id)
+    const { id }: { id: string } = req.params as { id: string }
+    
     const updates: string[] = Object.keys(req.body)
     const allowedUpdates: string[] = ['firstName', 'lastName', 'password', 'age']
     const isAllowed: boolean = updates.every((update: string) => allowedUpdates.includes(update))
 
-    if(!user) {
-        return res.status(404).send()
-    } else if (!isAllowed) {
-        return res.status(400).send({ error: 'Invalid updates'})
+    if(!isAllowed) {
+        return res.status(400).send()
+    } 
+
+    try {
+        const user: IUser | null = await User.findById(id)
+        if(!user) {
+            return res.status(404).send()
+        }
+        const updatedUser: IUser | null = await User.findByIdAndUpdate(id, req.body , { runValidators: true, new: true })
+        res.send(updatedUser)
+    } catch(error) {
+        res.status(500).send()
     }
-
-    const updatedUser: any = await User.findByIdAndUpdate(id, req.body , { runValidators: true, new: true })
-    res.send(updatedUser)
-
 })
 
 router.delete('/users/:id', async (req: Request, res: Response) => {
