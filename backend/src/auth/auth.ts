9a7c64b898f0fd2a825537b0ae01820a5bbcd285
Pranav Talwar <talwar.pranav123@gmail.com>
2020-06-25 18:48:26 +0530

    added authentication

---
+++ backend/src/auth/auth.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express'
+import * as jwt from 'jsonwebtoken'
+// import  } from 'jsonwebtoken'
+import User from '../models/User'
+import { IUserDocument } from '../models/UserInterfaces'
+import { decode } from 'punycode'
+
+type tokenInterface = Object & {
+    _id: string
+}
+
+
+const auth = async (req: Request, res: Response, next: NextFunction) => {
+    try {  
+        const token: string | undefined= req.headers.authorization?.replace('Bearer ', '')
+        let decodedToken: any
+        if(typeof token === 'string') {
+            decodedToken = jwt.verify(token, 'secret')
+        } else {
+            throw new Error()
+        }
+        
+        const user: IUserDocument | null = await User.findOne({ _id: decodedToken._id, 'tokens.token': token })
+        if(!user) {
+            throw new Error()
+        }
+
+        res.locals.user = user
+        res.locals.token = token
+        next()
+    } catch(error) {
+        res.status(401).send({ error: 'Please authenticate'})
+    }
+    
+    
+    next()
+}
+
+export default auth 
