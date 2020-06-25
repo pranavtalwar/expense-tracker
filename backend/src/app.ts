386c6344ce100738508b562d90d271f76e30ac4a
Pranav Talwar <talwar.pranav123@gmail.com>
2020-06-26 00:43:41 +0530

    added request logger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,18 @@
 import express, { Express, Request, Response } from 'express'
 import './db/db'
 import userRouter from './routers/userRouter'
+import expenseRouter from './routers/expenseRouter'
+
 
 const app: Express = express()
 
 app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.method, req.path)
+    next()
+})
 app.use(userRouter)
+app.use(expenseRouter)
+
 
 export { app }
