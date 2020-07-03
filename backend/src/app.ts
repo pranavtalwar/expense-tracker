57bc53d50a1f38d4aadc23e60d85b104d1b5939f
Pranav Talwar <talwar.pranav123@gmail.com>
2020-07-03 16:19:58 +0530

    added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express, Request, Response, NextFunction } from 'express'
+import cors from 'cors'
 import './db/db'
 import userRouter from './routers/userRouter'
 import expenseRouter from './routers/expenseRouter'
@@ -7,7 +8,8 @@ import expenseRouter from './routers/expenseRouter'
 const app: Express = express()
 
 app.use(express.json())
-app.use((req, res, next) => {
+app.use(cors())
+app.use((req: Request, res: Response, next: NextFunction): void => {
     console.log(req.method, req.path)
     next()
 })
